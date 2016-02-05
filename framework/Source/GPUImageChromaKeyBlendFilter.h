c78d465af430b0b49aa0d369f2f4d02052d66c4d
chikama <yasu.chikama@gmail.com>
2016-02-05 18:48:17 +0900

    コミット

--- framework/Source/GPUImageChromaKeyBlendFilter.h
+++ framework/Source/GPUImageChromaKeyBlendFilter.h
@@ -7,6 +7,16 @@
     GLint colorToReplaceUniform, thresholdSensitivityUniform, smoothingUniform;
 }
 
+// You can either set the transform to apply to be a 2-D affine transform or a 3-D transform. The default is the identity transform (the output image is identical to the input).
+@property(readwrite, nonatomic) CGAffineTransform affineTransform;
+@property(readwrite, nonatomic) CATransform3D transform3D;
+
+// This applies the transform to the raw frame data if set to YES, the default of NO takes the aspect ratio of the image input into account when rotating
+@property(readwrite, nonatomic) BOOL ignoreAspectRatio;
+
+// sets the anchor point to top left corner
+@property(readwrite, nonatomic) BOOL anchorTopLeft;
+
 /** The threshold sensitivity controls how similar pixels need to be colored to be replaced
  
  The default value is 0.3
